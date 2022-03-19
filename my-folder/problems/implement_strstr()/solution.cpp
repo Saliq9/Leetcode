7a664642b2d9627666c48c42cbf3b8709a21c357
Abdul Saliq Y <84172708+Saliq9@users.noreply.github.com>
2022-03-19 10:07:22 +0000

    Sync LeetCode submission - Implement strStr() (cpp)

---
+++ my-folder/problems/implement_strstr()/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int i = 0, n = haystack.size(), m = needle.size();
+        
+        if (m == 0) 
+          return 0;
+        
+        while (i + m - 1 < n) 
+        {    
+            if (haystack.substr(i, m) == needle)
+                return i;     
+            while (i++ + m - 1 < n && haystack[i] != needle[0]);
+        }   
+        return -1;
+    }
+};
