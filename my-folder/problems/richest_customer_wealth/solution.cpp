255a2d8453d91c451a8c5f0947a584c012bf1f9d
Abdul Saliq Y <84172708+Saliq9@users.noreply.github.com>
2022-06-01 09:32:22 +0000

    Sync LeetCode submission - Richest Customer Wealth (cpp)

---
+++ my-folder/problems/richest_customer_wealth/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) 
+    {
+        int sum = 0;
+        int ans = INT_MIN;
+        for(int i=0; i<accounts.size(); i++)
+        {
+            sum = 0;
+            for( int j=0; j<accounts[0].size();j++)
+            {
+                sum+=accounts[i][j];
+            }
+            ans= max(ans,sum);
+        }
+        return ans;
+    }
+};
