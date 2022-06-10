46635da865a4271c02ab43692d8294c8e9be9400
Abdul Saliq Y <84172708+Saliq9@users.noreply.github.com>
2022-06-10 10:16:37 +0000

    Sync LeetCode submission - Plus One (cpp)

--- my-folder/problems/plus_one/solution.cpp
+++ my-folder/problems/plus_one/solution.cpp
@@ -2,16 +2,19 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
       int n = digits.size() - 1;
-
-      for (int i = n; i >= 0; --i) 
+      
+      for (int i = n; i >= 0; --i)
       {
-        if (digits[i] == 9) {  
+        if (digits[i] == 9) 
+        {  
           digits[i] = 0;
-        } else {  
+        } else 
+        {  
           digits[i] += 1;
           return digits;
         }
       }
+
       digits.push_back(0);
       digits[0] = 1;
       return digits;
