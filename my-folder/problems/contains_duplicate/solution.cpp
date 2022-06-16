9a1b98a24e3ec15dad50658ea10ab09a1dcefb5a
Abdul Saliq Y <84172708+Saliq9@users.noreply.github.com>
2022-06-16 07:23:45 +0000

    Sync LeetCode submission - Contains Duplicate (cpp)

--- my-folder/problems/contains_duplicate/solution.cpp
+++ my-folder/problems/contains_duplicate/solution.cpp
@@ -1,14 +1,12 @@
 class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) 
-    {
-        sort(nums.begin(), nums.end());
-        
-        for(int i = 1; i < nums.size(); i++)
+		public:
+		bool containsDuplicate(vector<int>& nums) 
         {
-            if(nums[i-1] == nums[i])
-                return true;
-        }
-        return false;
+			sort(nums.begin(),nums.end());
+            
+			for(int i=1;i<nums.size();i++)
+				if(nums[i]==nums[i-1])
+					return true;
+			return false;
     }
 };
