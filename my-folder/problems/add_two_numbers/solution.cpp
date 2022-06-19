f8d1650fbec3aa3623e343b09ad21449092493de
Abdul Saliq Y <84172708+Saliq9@users.noreply.github.com>
2022-06-19 08:11:21 +0000

    Sync LeetCode submission - Add Two Numbers (cpp)

---
+++ my-folder/problems/add_two_numbers/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* p, ListNode* q) 
+    {
+
+        if(p == nullptr && q == nullptr) 
+            return nullptr;
+		
+        int sum = 0;
+        if(p != nullptr)
+        {
+            sum += p->val;
+            p = p->next;
+        }
+        if(q != nullptr){
+            sum += q->val;
+            q = q->next;
+        }
+        
+        ListNode* res = new ListNode(sum%10);
+
+        if(sum > 9)
+        {
+            if(p != nullptr) p->val ++;
+            else if(q != nullptr) q->val ++;
+            else
+            {
+                res->next = new ListNode(sum/10);
+                return res;
+            }
+        }
+        res->next = addTwoNumbers(p, q);
+        
+        return res;
+    }
+};
