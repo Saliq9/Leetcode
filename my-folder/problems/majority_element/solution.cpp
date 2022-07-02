eb14dd066088d61b59d2d9f4b4b319a66f57182b
Abdul Saliq Y <84172708+Saliq9@users.noreply.github.com>
2022-07-02 06:46:20 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- my-folder/problems/majority_element/solution.cpp
+++ my-folder/problems/majority_element/solution.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
+		
         sort(nums.begin(),nums.end());
-            
-        return nums[n/2]; // Number occuring more than half the size of array 
+		
+        return nums[n/2];
+        
     }
 };
